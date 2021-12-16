bb38a79195ac7809b53b9080cb71f7fb9322da5b
Manikanta Mundru <Manikanta.Mundru@rapiddeploy.com>
2021-12-16 15:38:00 +0200

    semaphore slim  for limiting threads to prevent dead locks

---
+++ Semephore/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace SemaphoreExample
+{
+    class Program
+    {
+        public static Semaphore semaphore = new Semaphore(2, 3);
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Thread threadObject = new Thread(DoSomeTask)
+                {
+                    Name = "Thread " + i
+                };
+                threadObject.Start(i);
+            }
+            Console.ReadKey();
+        }
+
+        static void DoSomeTask(object id)
+        {
+            Console.WriteLine(Thread.CurrentThread.Name + " Wants to Enter into Critical Section for processing");
+            try
+            {
+                //Blocks the current thread until the current WaitHandle receives a signal.   
+                semaphore.WaitOne();
+                Console.WriteLine("Success: " + Thread.CurrentThread.Name + " is Doing its work");
+                Thread.Sleep(5000);
+                Console.WriteLine(Thread.CurrentThread.Name + "Exit.");
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+    }
+}
